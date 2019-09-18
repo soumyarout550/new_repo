7f097a220b7bf734755a45c8803f693b71219ca3
soumyarout550 <srout8431@gmail.com>
2019-09-18 16:27:04 +0530

    file added

---
+++ src/main/java/com/maaven/practice/App.java
@@ -0,0 +1,29 @@
+package com.maaven.practice;
+
+/**
+ * WriteToFile!
+ *
+ */
+
+import java.io.PrintStream;
+import java.io.IOException;
+import java.io.File;
+
+
+public class App 
+{	public static void main(String[] args)
+	{	try
+		{	PrintStream writer = new PrintStream( new File("randInts.txt"));
+			Random r = new Random();
+			final int LIMIT = 100;
+
+			for(int i = 0; i < LIMIT; i++)
+			{	writer.println( r.nextInt() );
+			}
+			writer.close();
+		}
+		catch(IOException e)
+		{	System.out.println("An error occured while trying to write to the file");
+		}
+	}
+}
